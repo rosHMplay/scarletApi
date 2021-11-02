9d1fdbe7638c9350a6a7ed5a6683832ffe48f17a
Ronald Oliveira <roshmplay.github.com.br>
2021-11-02 12:17:43 -0300

    project

---
+++ Controllers/EntityTask.cs
@@ -0,0 +1,9 @@
+﻿namespace ScarletAPI.Controllers
+{
+    public class EntityTask
+    {
+        public string Title { get; internal set; }
+        public string Description { get; internal set; }
+        public object Items { get; internal set; }
+    }
+}
---
+++ Controllers/EntityTaskController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ScarletAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EntityTaskController : ControllerBase
+    {
+        public EntityTaskController()
+        {
+
+        }
+
+        [HttpGet]
+        public IEnumerable<EntityTask> Get()
+        {
+            return new List<EntityTask>()
+            {
+                new EntityTask
+                {
+                    Title = "Despertar",
+                    Description = "Comece bem o seu dia",
+                    Items = new string[] {"Levantar 05:50" , "Beba água", "Tome um banho gelado", "Refeição um" }
+                }
+            };
+        }
+    }
+}
